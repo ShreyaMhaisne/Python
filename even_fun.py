67eb8cfdd543e7cebc8d678efe6d69cbf88e56ac
Shreya Mhaisne <pshreeya050@gmail.com>
2025-06-17 13:37:26 +0530

    Add files via upload

---
+++ even_fun.py
@@ -0,0 +1,7 @@
+def even(a):
+    if a%2==0:
+        return "even"
+    else:
+        return "odd"
+
+print(even(3))
---
+++ square.py
@@ -0,0 +1,5 @@
+def square(num):
+    return num * num
+
+result = square(2)
+print("square ",result)
